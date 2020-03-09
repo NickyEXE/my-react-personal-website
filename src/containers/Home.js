ed46f6393d3fd4bac7dfaae6eed7d1cb985e1e75
NickyEXE <dovern42@gmail.com>
2020-03-09 11:40:29 -0400

    pulled jumbotron into own component

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,16 +1,11 @@
 import React, { Component } from "react";
-import profileJumbotron from '../assets/profileJumbotron.png'
-
+import Jumbotron from '../components/Jumbotron.js'
 class Home extends Component {
 
 
     render(){
         return (
-            <div className="jumbotron">
-                <img id="profileJumbotron" src={profileJumbotron} alt="Picture of Nicky"/>
-                <h1><span className="subway m-train">M</span>eet <span className="subway n-train">N</span>icky</h1>
-                <h3>Nicky is from New York City. He likes to build cool things.</h3>
-            </div>
+            <Jumbotron/>
         )
     }
 
