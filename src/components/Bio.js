49570c8464cca07d84f29d99495f66afb08367da
NickyEXE <dovern42@gmail.com>
2020-03-09 14:42:19 -0400

    did some css unwonking

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,12 +1,12 @@
 import React from "react";
-import profileJumbotron from '../assets/photos/profileJumbotron.png'
 
 const Bio = () => {
     return(
         <div className="bio">
             <div className="bio-entries">
             <p>Nicky is a full-stack developer with a specialty in MVC frameworks.</p>
-            <p>He currently coaches Ruby, Rails, JavaScript, and React at the Flatiron School in Brooklyn.</p>
+            <p>He currently coaches programming at the Flatiron School in Brooklyn.</p>
+            <p>He teaches Ruby, Rails, JavaScript, and React.</p>
             </div>
         </div>
     )
