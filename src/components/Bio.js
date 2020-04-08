2ef9665c56b9cf1a3bc4a6c0e22b31323a13257d
NickyEXE <dovern42@gmail.com>
2020-04-08 16:27:58 -0400

    correction in bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -7,7 +7,7 @@ const Bio = () => {
                 <p>Nicky is a full-stack developer with a specialty in MVC frameworks.</p>
                 <p>He currently coaches programming at the Flatiron School in Brooklyn. He graduated from Flatiron School in June 2019.</p>
                 <p>He teaches Ruby, Rails, JavaScript, and React.</p>
-                <p>Before coding, he was a Public Relations consultant for Edelman for five years.</p>
+                <p>Before coding, he was a Public Relations consultant for Edelman for four years.</p>
             </div>
         </div>
     )
