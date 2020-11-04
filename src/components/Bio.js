bbe8823e11e654369d096753243fc983cc37c6d8
Nicky_Dover.exe <dovern42@gmail.com>
2020-11-04 18:45:53 -0500

    Updates my job description

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -5,7 +5,7 @@ const Bio = () => {
         <div className="bio">
             <div className="bio-entries">
                 <p>Nicky is a full-stack developer with a specialty in MVC frameworks.</p>
-                <p>He currently coaches programming at the Flatiron School in Brooklyn. He graduated from Flatiron School in June 2019.</p>
+                <p>He currently leads cohorts and teaches classes at Flatiron School, which he graduated from in June 2019.</p>
                 <p>He teaches Ruby, Rails, JavaScript, and React.</p>
                 <p>Before coding, he was a Public Relations consultant for Edelman for four years.</p>
             </div>
