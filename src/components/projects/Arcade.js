d44f17a854fa8aaec76379423b0ccf666c1d928f
NickyEXE <dovern42@gmail.com>
2021-09-16 13:15:16 -0400

    add pong

--- src/components/projects/Arcade.js
+++ src/components/projects/Arcade.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import playPong from '../../games/pong/pong';
 
 const Arcade = () => {
-
-  return `
-  <div id="arcade">
+  return (<div id="arcade">
     <p>This portion of the website works best in the browser.</p>
-    <div id="arcade-main"></div>
-  </div>`
-
+    <canvas id="canvas"></canvas>
+    <br/>
+    <button onClick={playPong}>Pong</button>
+  </div>)
 }
 
 export default Arcade;
---
+++ src/games/pong/Paddle.js
@@ -0,0 +1,35 @@
+export default class Paddle {
+  constructor(direction, ctx, config, canvas){
+      this.direction = direction
+      this.y = config.canvasHeight/2 - config.paddleHeight/2
+      direction === 1 ? this.x = 0 : this.x = config.canvasWidth - config.paddleWidth
+      this.score = 0
+      this.ctx = ctx
+      this.config = config
+      this.canvas = canvas
+  }
+
+  render = () => {
+      this.ctx.fillStyle = "#FF0000";
+      this.ctx.fillRect(this.x, this.y, this.config.paddleWidth, this.config.paddleHeight);
+      this.ctx.font = "30px Arial";
+      this.ctx.fillText(this.score, this.canvas.width/2 + (200 * this.direction), 50);
+  }
+
+  checkCollision = (ball) => {
+      console.log("checking")
+      return !!((this.y <= (ball.y + ball.r)) && (this.y + this.config.paddleHeight >= (ball.y - ball.r)))
+  }
+
+  moveDown = () => {
+      this.y += this.config.paddleSpeed;
+      (this.y + this.config.paddleHeight > this.canvas.height) && (this.y = this.canvas.height - this.config.paddleHeight)
+  }
+
+  moveUp = () => {
+      this.y -= this.config.paddleSpeed
+      this.y < 0 && (this.y = 0)
+  }
+
+  win = () => this.score ++
+}
