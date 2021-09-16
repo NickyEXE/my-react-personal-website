abbafbb8605a44d22c9fd2a6140dff263be9e8ea
NickyEXE <dovern42@gmail.com>
2021-09-16 16:12:10 -0400

    refactor to use react tools rather than vanilla js

--- src/components/projects/Arcade.js
+++ src/components/projects/Arcade.js
@@ -1,23 +1,22 @@
 import { render } from '@testing-library/react';
+import { useState } from 'react';
 import React from 'react';
 import playPong from '../../games/pong/pong';
+import { Route, Switch} from 'react-router-dom';
 
 const Arcade = () => {
 
+  const [selected, setSelected] = useState(null)
+
   const renderArcade = (gameCallback) => {
-    const arcadeMachine = document.querySelector(".arcade-machine")
-    arcadeMachine.classList.add("on")
-    arcadeMachine.classList.add("neo-geo")
-    const arcadeHeader = document.querySelector(".arcade-header")
-    arcadeHeader.innerHTML = `<h1>NICKY-DOVER™️</h1>`
-    arcadeHeader.classList.add("neo-geo")
+    setSelected("pong")
     gameCallback(document.getElementById("arcade-main"))
   }
 
   return (<div id="arcade">
     <p>This portion of the website works best in the browser.</p>
-    <div class="arcade-machine">
-    <div class="arcade-header"></div>
+    <div class={`arcade-machine ${selected ? "on neo-geo" : ""}`}>
+    <div class="arcade-header">{selected && <h1>NICKY-DOVER™️</h1>}</div>
     <div id="arcade-main"></div>
     </div>
     <br/>
