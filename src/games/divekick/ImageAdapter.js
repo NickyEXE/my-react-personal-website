7ae7d1dc91f31a37cdfb01e4ab67c864ee7cfbbd
NickyEXE <dovern42@gmail.com>
2021-09-17 01:02:02 -0400

    forgetting that react has a public asset pipeline, are you?

--- src/games/divekick/ImageAdapter.js
+++ src/games/divekick/ImageAdapter.js
@@ -1,12 +1,12 @@
-import gracieRightFacing from "./assets/gracie-right-facing.png"
-import gracieLeftFacing from "./assets/gracie-left-facing.png"
-import gracieRightKod from "./assets/gracie-right-kod.png"
-import gracieLeftKod from "./assets/gracie-left-kod.png"
-import nickyRightFacing from "./assets/nicky-right-facing.jpeg"
-import nickyLeftFacing from "./assets/nicky-left-facing.jpeg"
-import nickyRightKod from "./assets/nicky-right-kod.jpeg"
-import nickyLeftKod from "./assets/nicky-left-kod.jpeg"
-import background from "./assets/class2.jpg"
+import gracieRightFacing from "../../assets/divekick/gracie-right-facing.png"
+import gracieLeftFacing from "../../assets/divekick/gracie-left-facing.png"
+import gracieRightKod from "../../assets/divekick/gracie-right-kod.png"
+import gracieLeftKod from "../../assets/divekick/gracie-left-kod.png"
+import nickyRightFacing from "../../assets/divekick/nicky-right-facing.jpeg"
+import nickyLeftFacing from "../../assets/divekick/nicky-left-facing.jpeg"
+import nickyRightKod from "../../assets/divekick/nicky-right-kod.jpeg"
+import nickyLeftKod from "../../assets/divekick/nicky-left-kod.jpeg"
+import background from "../../assets/divekick/class2.jpg"
 
 const imageAdapter = {background, gracieLeftFacing, gracieRightFacing, gracieRightKod, gracieLeftKod, nickyLeftFacing, nickyRightFacing, nickyRightKod, nickyLeftKod}
 
