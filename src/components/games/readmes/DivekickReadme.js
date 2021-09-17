0f7718f735ffd54f50d744f0496f2ab529107cc3
NickyEXE <dovern42@gmail.com>
2021-09-17 00:57:56 -0400

    add WIP note on Divekick

--- src/components/games/readmes/DivekickReadme.js
+++ src/components/games/readmes/DivekickReadme.js
@@ -9,8 +9,8 @@ export default function DivekickReadme(){
   <p>There are two buttons:</p>
   <p><strong>Dive (P1: A, P2: K):</strong> The game's term for "jump", which sends you straight into the air.</p>
   <p><strong>Kick (P1: S, P2: L):</strong> When jumping, this will send you flying downwards towards the enemy. When on the ground, it will perform a "kickback", allowing you to jump backwards in an arc.</p>
-  <p></p>
   <p>I built this early on as a coach at Flatiron School, and this clone involves me battling my co-coach at the time, Gracie McGuire.</p>
   <p>(This is flat bad salesmanship, but if you're a potential employer, you'd be remiss if you didn't also check out <a href="https://graciemcguire.com/#/">Gracie's website here.</a>)</p>
+  <p>Note: As of writing this, it's showing up as a black screen when deployed on AWS. I'm working on it! If it's not a black screen, it means I fixed it and forgot about this line.</p>
   </>
 }
