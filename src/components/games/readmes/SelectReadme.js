6e84fd114977198e62e1479f254d3df6c95e936e
NickyEXE <dovern42@gmail.com>
2021-09-17 01:33:32 -0400

    youtube embed attribute names to JSX

--- src/components/games/readmes/SelectReadme.js
+++ src/components/games/readmes/SelectReadme.js
@@ -9,7 +9,7 @@ export default function SelectReadme(){
   <p>Click one of the game icons above the arcade machine to get started.</p>
   <p>I've also taught lectures on the basics of HTML5 canvas.</p>
   <p>Watch me talk about it!</p>
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/vQCwh2odc4Q?start=18" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/vQCwh2odc4Q?start=18" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
   <p>A couple of these apps have been refactored and changed to fit this Neo-Geo, so you'll find the up-to-date code on this website's repo <a href="https://github.com/NickyEXE/my-react-personal-website/tree/master/src/games">here.</a></p>
   </>
 
